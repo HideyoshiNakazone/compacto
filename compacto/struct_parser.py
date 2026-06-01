214c72640ecc8f74a67052fb862dc90528797a23
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2026-06-01 14:10:52 -0300

    feat: implement struct_parser for type annotations and tree structure

---
+++ compacto/struct_parser.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+from compacto.internal_types import HasAnnotations, TreeNode
+
+from typing import TypeVar, get_type_hints
+
+
+T = TypeVar("T", bound=HasAnnotations)
+
+
+@dataclass
+class StructDeff:
+    name: str
+    native_type: type
+    fields: dict[str, type]
+
+
+@dataclass
+class FieldsDeff:
+    name: str
+    field_type: type
+
+
+StructTyping = StructDeff | FieldsDeff
+
+
+def struct_parser(obj_or_clzz: T | type[T]) -> TreeNode[StructTyping]:
+    clzz = obj_or_clzz if isinstance(obj_or_clzz, type) else type(obj_or_clzz)
+
+    annotated_fields = get_type_hints(clzz)
+    if not annotated_fields:
+        raise TypeError("Expected clzz to have a __annotations__ attribute")
+
+    all_attrs = [
+        attr
+        for attr in dir(clzz)
+        if not attr.startswith("__") and not callable(getattr(clzz, attr))
+    ]
+    for field in all_attrs:
+        if field not in annotated_fields:
+            raise TypeError(f"Field {field} not in {annotated_fields}")
+
+    struct_node = TreeNode[StructTyping].new(
+        StructDeff(name=clzz.__name__, native_type=clzz, fields=annotated_fields)
+    )
+    for field, field_type in annotated_fields.items():
+        if getattr(field_type, '__module__', None) == 'builtins':
+            struct_node.add_child(
+                TreeNode.new(FieldsDeff(name=field, field_type=field_type))
+            )
+        else:
+            struct_node.add_child(
+                struct_parser(clzz)
+            )
+
+    return struct_node
---
+++ tests/test_struct_parser.py
@@ -0,0 +1,29 @@
+from compacto.struct_parser import struct_parser, StructDeff, FieldsDeff
+
+import pytest
+
+
+def test_struct_parser_throws_if_no_annotations() -> None:
+    with pytest.raises(TypeError):
+        struct_parser({})
+
+
+def test_struct_parser_throws_if_annotations_are_missing() -> None:
+    with pytest.raises(TypeError):
+
+        class NoAnnotations:
+            a: str
+            b = None
+
+        struct_parser(NoAnnotations)
+
+
+def test_struct_parser_valid_annotations() -> None:
+    class ValidAnnotations:
+        a: str
+        b: int
+
+    typing_tree = struct_parser(ValidAnnotations)
+
+    assert isinstance(typing_tree.data, StructDeff)
+    assert all(isinstance(child.data, FieldsDeff) for child in typing_tree.children)
