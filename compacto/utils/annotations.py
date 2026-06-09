bef6ea2d800b16b3ffd12999497fd78bbc58c8d7
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2026-06-08 23:54:06 -0300

    feat: add runtime_checkable decorator to HasAnnotations protocol

--- compacto/utils/annotations.py
+++ compacto/utils/annotations.py
@@ -1,5 +1,6 @@
-from typing_extensions import Protocol
+from typing_extensions import Protocol, runtime_checkable
 
 
+@runtime_checkable
 class HasAnnotations(Protocol):
     __annotations__: dict[str, type]
